3f8942cd7b3cdc2dd8232691ef65402b71d67900
Damien Butt <damien.butt@norgateav.com>
2022-05-18 21:08:34 +0100

    chore: add hard button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { subscribeState } from "@crestron/ch5-crcomlib";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        subscribeState("b", "Csig.Hard_Button_1.Press", (value: boolean) => {
+            console.log("Hard Button 1 Pressed: ", value);
+        });
+        subscribeState("b", "Csig.Hard_Button_2.Press", (value: boolean) => {
+            console.log("Hard Button 2 Pressed: ", value);
+        });
+        subscribeState("b", "Csig.Hard_Button_3.Press", (value: boolean) => {
+            console.log("Hard Button 3 Pressed: ", value);
+        });
+        subscribeState("b", "Csig.Hard_Button_4.Press", (value: boolean) => {
+            console.log("Hard Button 4 Pressed: ", value);
+        });
+        subscribeState("b", "Csig.Hard_Button_5.Press", (value: boolean) => {
+            console.log("Hard Button 5 Pressed: ", value);
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+    bridgeReceiveIntegerFromNative,
+    bridgeReceiveBooleanFromNative,
+    bridgeReceiveStringFromNative,
+    bridgeReceiveObjectFromNative,
+    isCrestronTouchscreen,
+} from "@crestron/ch5-crcomlib";
+import eruda from "eruda";
+
+if (isCrestronTouchscreen()) {
+    (window as any).bridgeReceiveIntegerFromNative =
+        bridgeReceiveIntegerFromNative;
+    (window as any).bridgeReceiveBooleanFromNative =
+        bridgeReceiveBooleanFromNative;
+    (window as any).bridgeReceiveStringFromNative =
+        bridgeReceiveStringFromNative;
+    (window as any).bridgeReceiveObjectFromNative =
+        bridgeReceiveObjectFromNative;
+
+    if (process.env.REACT_APP_ENV !== "production") {
+        eruda.init({
+            autoScale: false,
+            defaults: {
+                theme: "Dracula",
+            },
+        });
+
+        eruda.scale(1.5);
+    }
+}
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
